dcc11408ae5036da7cee106074eea03990d271cb
Gabriela <gabrielailarreta@gmail.com>
2018-09-04 15:39:01 -0300

    Parte 2: Create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,12 +4,18 @@ class TodosController < ApplicationController
 	end
 
 	def new
-		@product = Product.new
+		@todo = Todo.new
 	end
 
-	
+	def create
+		@todo = Todo.new(todo_params)
+		@todo.save
+    	redirect_to todos_path
+	end
+
 	private
 	def todo_params
 		params.require(:todo).permit(:description, :completed)
 	end
+	
 end
