2d55ae2d55fc7eecaf454de7020838c8636dd3c2
Gabriela <gabrielailarreta@gmail.com>
2018-09-04 14:48:32 -0300

    Parte 1: Index

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,10 @@
 class TodosController < ApplicationController
+	def index
+		@todos = Todo.all
+	end
+
+	private
+	def todo_params
+		params.require(:todo).permit(:description, :completed)
+	end
 end
