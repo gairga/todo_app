09b45d7ab4f19783ff8bb2c65b5f12788e002ac1
Gabriela <gabrielailarreta@gmail.com>
2018-09-05 00:45:14 -0300

    Parte 3: Show,Edit,Update,Destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,6 +13,37 @@ class TodosController < ApplicationController
     	redirect_to todos_path
 	end
 
+	def show
+		@todo = Todo.find(params[:id])
+	end
+
+	def edit
+    	@todo = Todo.find(params[:id])
+ 	end
+
+	def update
+	    @todo = Todo.find(params[:id])
+	    @todo.update(todo_params)
+	    redirect_to todos_path
+	end
+
+	def destroy
+		@todo = Todo.find(params[:id])
+		@todo.destroy
+		redirect_to todos_path
+	end
+
+	def complete
+		@todo = Todo.find(params[:id])
+		@todo.completed = true
+		@todo.save
+		redirect_to todos_path
+  	end
+
+  	def list
+    	@todos = Todo.all
+  	end
+
 	private
 	def todo_params
 		params.require(:todo).permit(:description, :completed)
